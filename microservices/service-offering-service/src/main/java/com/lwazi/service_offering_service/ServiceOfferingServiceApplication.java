60e649b608cce1ba88f653213bd535e2ecdb77b7
lwaziT <61688164+lwaziT@users.noreply.github.com>
2025-02-09 22:33:22 +0200

    Added service - Generated new service-offering-service folder.

---
+++ microservices/service-offering-service/src/main/java/com/lwazi/service_offering_service/ServiceOfferingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.lwazi.service_offering_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceOfferingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceOfferingServiceApplication.class, args);
+	}
+
+}
---
+++ microservices/service-offering-service/src/test/java/com/lwazi/service_offering_service/ServiceOfferingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lwazi.service_offering_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceOfferingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
